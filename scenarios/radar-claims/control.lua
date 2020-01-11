1faf413822cc0ca983171a6a6afddc8890ef26bd
Ben Buckton <ben.buckton@factorio.com>
2020-01-11 11:53:04 +0100

    added no-military package

--- scenarios/radar-claims/control.lua
+++ scenarios/radar-claims/control.lua
@@ -1,5 +1,6 @@
 local handler = require("event_handler")
 local claims = require("radar-claims")
+local no_military = require("no-military")
 
 
 local on_created_or_loaded = function()
@@ -14,12 +15,36 @@ end
 
 local on_player_created = function(event)
   local player = game.players[event.player_index]
-  local new_force = game.create_force(player.name)
-  player.force = new_force
-  for force_name, force in pairs(game.forces) do
-    if force_name ~= 'enemy' then
+  if event.player_index == 1 then
+
+  else
+    local new_force = game.create_force(player.name)
+    player.force = new_force
+    for force_name, force in pairs(game.forces) do
+      if force_name ~= 'enemy' then
       force.set_friend(new_force,true)
       new_force.set_friend(force,true)
+    end
+  end
+end
+
+local on_chunk_generated = function(event)
+  --if this chunk contains stone deposits, make a mixed deposit
+  local resources = event.surface.find_entities_filtered({
+    type = 'resource',
+    area = event.area
+  })
+  local valid_swaps = {'iron-ore','copper-ore','coal','stone'}
+
+  for _, resource in pairs(resources) do
+     local random_type = valid_swaps[math.random(1,#valid_swaps)]
+    if random_type ~= resource.name then
+      local new_dep = event.surface.create_entity({
+        name = random_type,
+        position = resource.position,
+      })
+      new_dep.amount = resource.amount
+      resource.destroy()
     end
   end
 end
@@ -27,10 +52,12 @@ end
 local main_events = {
   [defines.events.on_game_created_from_scenario] = on_game_created_from_scenario,
   [defines.events.on_player_created] = on_player_created,
+  [defines.events.on_chunk_generated] = on_chunk_generated,
 }
 
 handler.add_lib({events = main_events})
 handler.add_lib(claims)
+handler.add_lib(no_military)
 
 script.on_load(on_created_or_loaded)
 
---
+++ scenarios/radar-claims/no-military.lua
@@ -0,0 +1,101 @@
+local no_military = {}
+
+local on_game_created_from_scenario = function()
+  global.no_military_data = {}
+
+  local group = game.permissions.create_group('pacifists')
+  group.set_allows_action(defines.input_action.change_picking_state,true)
+  group.set_allows_action(defines.input_action.change_shooting_state,true)
+  group.set_allows_action(defines.input_action.open_kills_gui,true)
+  group.set_allows_action(defines.input_action.select_next_valid_gun,true)
+  group.set_allows_action(defines.input_action.set_car_weapons_control,true)
+  group.set_allows_action(defines.input_action.wire_dragging,true)
+  group.set_allows_action(defines.input_action.use_artillery_remote,true)
+  group.set_allows_action(defines.input_action.use_item,true)
+
+  global.no_military_data.banned_technologies = {
+
+
+  }
+
+  global.no_military_data.banned_recipes = {
+    'pistol',
+    'firearm-magazine'
+  }
+  local settings = game.surfaces['nauvis'].map_gen_settings
+  settings.peaceful_mode = true
+  settings.autoplace_controls['enemy-base'] = {
+    frequency = 0,
+    richness = 0,
+    size = 0
+  }
+
+  game.surfaces['nauvis'].map_gen_settings = settings
+  --game.surfaces['nauvis'].clear()
+
+  game.map_settings.pollution.enabled = false
+  game.map_settings.enemy_evolution.enabled = false
+  game.map_settings.enemy_expansion.enabled = false
+end
+
+local on_player_created = function(event)
+  local player = game.players[event.player_index]
+  if event.player_index == 1 then
+    player.print("Welcome "..player.name.. ". You have been made a moderator")
+  else
+    player.print("Welcome "..player.name)
+    game.permissions.get_group('pacifists').add_player(player)
+    for _, item_name in pairs(global.no_military_data.banned_recipes) do
+      player.remove_item({name=item_name,count=9999})
+    end
+  end
+end
+
+local on_player_respawned = function(event)
+  local player = game.players[event.player_index]
+  for _, item_name in pairs(global.no_military_data.banned_recipes) do
+      player.remove_item({name=item_name,count=9999})
+  end
+end
+
+local on_force_created = function(event)
+  for _, technology_name in pairs(global.no_military_data.banned_technologies) do
+    if event.force.technologies[technology_name] then
+      event.force.technologies[technology_name].enabled = false
+    end
+  end
+  for _, recipe_name in pairs(global.no_military_data.banned_recipes) do
+    if event.force.recipes[recipe_name] then
+      event.force.recipes[recipe_name].enabled = false
+    end
+  end
+end
+
+local on_research_finished = function(event)
+  for _, recipe_name in pairs(global.no_military_data.banned_recipes) do
+    if event.research.force.recipes[recipe_name] then
+      event.research.force.recipes[recipe_name].enabled = false
+    end
+  end
+end
+
+local on_chunk_generated = function(event)
+  local enemies = event.surface.find_entities_filtered({
+    force = 'enemy',
+    area = area,
+  })
+  for _, ent in pairs(enemies) do
+    ent.destroy()
+  end
+end
+
+no_military.events = {
+  [defines.events.on_game_created_from_scenario] = on_game_created_from_scenario,
+  [defines.events.on_player_created] = on_player_created,
+  [defines.events.on_player_respawned] = on_player_respawned,
+  [defines.events.on_force_created] = on_force_created,
+  [defines.events.on_research_finished] = on_research_finished,
+  [defines.events.on_chunk_generated] = on_chunk_generated,
+}
+
+return no_military
